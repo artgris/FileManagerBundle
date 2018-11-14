2fdb6a8904bdcb366fad13eb2ec01747ebd9e089
Josep Cortes <josepcortes.work@gmail.com>
2018-11-14 17:13:43 +0100

    Setting trans key for the user media library root folder, to avoid showing uuid.

--- Controller/ManagerController.php
+++ Controller/ManagerController.php
@@ -60,6 +60,12 @@ class ManagerController extends Controller
         // Folder search
         $directoriesArbo = $this->retrieveSubDirectories($fileManager, $fileManager->getDirName(), DIRECTORY_SEPARATOR, $fileManager->getBaseName());
 
+
+        if(isset($directoriesArbo[0]['text'])){
+            $directoriesArbo[0]['text'] = sprintf('%s <span class="label label-default">1</span>', $translator->trans('randock.ypsa.my_media_library.title'));
+        }
+
+
         // File search
         $finderFiles = new Finder();
         $finderFiles->in($fileManager->getCurrentPath())->depth(0);
