21f4f96ecb5fd246c169ce8de5f2f636574d769c
artgris <a.gribet@gmail.com>
2020-02-11 15:09:47 +0100

    fix composer

--- Tests/Fixtures/AbstractTestCase.php
+++ Tests/Fixtures/AbstractTestCase.php
@@ -10,7 +10,7 @@ abstract class AbstractTestCase extends WebTestCase
     /** @var Client */
     protected $client;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         $this->initClient();
     }
