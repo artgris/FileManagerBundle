60075a5d9c2dacf33943dc93d45340631bc18066
artgris <a.gribet@gmail.com>
2022-01-04 16:47:38 +0100

    composer install error

--- Helpers/FileManagerUploadHandler.php
+++ Helpers/FileManagerUploadHandler.php
@@ -4,7 +4,7 @@
 namespace Artgris\Bundle\FileManagerBundle\Helpers;
 
 
-class FileManagerUploadHandler extends \UploadHandler {
+class FileManagerUploadHandler extends UploadHandler {
 
     protected function get_unique_filename($file_path, $name, $size, $type, $error,
         $index, $content_range) {
