1d8708e9b9dcb3fa466a3044634f824b06d48363
artgris <a.gribet@gmail.com>
2023-04-11 08:53:25 +0200

    doctrine

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -8,4 +8,10 @@ if (!file_exists($file)) {
 }
 $autoload = require $file;
 
+AnnotationRegistry::registerLoader(function ($class) use ($autoload) {
+    $autoload->loadClass($class);
+
+    return class_exists($class, false);
+});
+
 include __DIR__.'/Fixtures/App/AppKernel.php';
