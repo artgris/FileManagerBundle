a11b6c3148c0abcd91ee3ce358f784a607077ba0
Sait KURT <33868586+xDeSwa@users.noreply.github.com>
2024-02-01 10:57:48 +0300

    File Name Sanitizer / Slugger (#134)

--- Helpers/FileManagerUploadHandler.php
+++ Helpers/FileManagerUploadHandler.php
@@ -4,10 +4,15 @@
 namespace Artgris\Bundle\FileManagerBundle\Helpers;
 
 
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
 class FileManagerUploadHandler extends UploadHandler {
 
     protected function get_unique_filename($file_path, $name, $size, $type, $error,
-        $index, $content_range) {
+                                           $index, $content_range) {
+
+        $name = $this->set_filename_sanitizer($name);
+
         if ($this->options['override']) {
             return $name;
         }
@@ -27,6 +32,46 @@ class FileManagerUploadHandler extends UploadHandler {
         }
 
         return $name;
+    }
+
+    protected function set_filename_sanitizer($name): string
+    {
+
+        if(!isset($this->options['filename_sanitizer']['slugger']) || $this->options['filename_sanitizer']['slugger'] !== true){
+            return $name;
+        }
+
+        $file_extension = pathinfo($name, PATHINFO_EXTENSION);
+        $new_file_name = str_replace('.' . $file_extension, '', $name);
+
+        $slugger = new AsciiSlugger();
+
+        if (isset($this->options['filename_sanitizer']['prepend'])) {
+            $new_file_name = $slugger->slug($new_file_name)->prepend($this->options['filename_sanitizer']['prepend']);
+        }
+
+        if (isset($this->options['filename_sanitizer']['append'])) {
+            $new_file_name = $slugger->slug($new_file_name)->append($this->options['filename_sanitizer']['append']);
+        }
+
+        if (isset($this->options['filename_sanitizer']['transformer'])) {
+            $new_file_name = match ($this->options['filename_sanitizer']['transformer']) {
+                'uppercase' => $slugger->slug($new_file_name)->upper(),
+                'lowercase' => $slugger->slug($new_file_name)->lower(),
+                'camel' => $slugger->slug($new_file_name)->camel(),
+                'snake' => $slugger->slug($new_file_name)->snake(),
+                'cameltitle' => $slugger->slug($new_file_name)->camel()->title(),
+                'title' => $slugger->slug($new_file_name)->title(),
+                'titleall' => $slugger->slug($new_file_name)->title(true),
+                'folded' => $slugger->slug($new_file_name)->folded(),
+                default => $slugger->slug($new_file_name),
+            };
+        } else {
+            $new_file_name = $slugger->slug($new_file_name);
+        }
+
+
+        return $new_file_name  . "." . $file_extension;
     }
 
 }
